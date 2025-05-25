b22df38805e067fe3f7cdc4d8ee36a3d3bab36ce
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-25 21:51:03 +0300

    Fix: Move devs and companies properties to correct models and setup session in debug

--- lib/models.py
+++ lib/models.py
@@ -1,15 +1,3 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import joinedload
-
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-
-Base = declarative_base(metadata=metadata)
-
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -17,44 +5,27 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
-    def __repr__(self):
-        return f'<Company {self.name}>'
-
-class Dev(Base):
-    __tablename__ = 'devs'
-
-    id = Column(Integer(), primary_key=True)
-    name= Column(String())
-
-    def __repr__(self):
-        return f'<Dev {self.name}>'
-    
-
-class Freebie(Base):
-    __tablename__ = 'freebies'
-
-    id = Column(Integer, primary_key=True)
-    item_name = Column(String)
-    value = Column(Integer)
-
-    dev_id = Column(Integer, ForeignKey('devs.id'))
-    company_id = Column(Integer, ForeignKey('companies.id'))
-
-    dev = relationship("Dev", backref="freebies")
-    company = relationship("Company", backref="freebies")
-
-    def __repr__(self):
-        return f"<Freebie {self.item_name} (${self.value})>"
+    freebies = relationship("Freebie", backref="company")
 
     @property
     def devs(self):
         return list({freebie.dev for freebie in self.freebies})
 
-    Company.devs = devs
+    def __repr__(self):
+        return f'<Company {self.name}>'
 
-    # Add this to Dev class
+
+class Dev(Base):
+    __tablename__ = 'devs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    freebies = relationship("Freebie", backref="dev")
+
     @property
     def companies(self):
         return list({freebie.company for freebie in self.freebies})
 
-    Dev.companies = companies
+    def __repr__(self):
+        return f'<Dev {self.name}>'
